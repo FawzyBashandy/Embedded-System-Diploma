20f7031fc259a9e5211660a95887c3a781fa946f
keroles shenouda <fawzybashandy@gmail.com>
2023-06-29 19:06:14 +0300

    Adding Ex_2-Assi_1

---
+++ Unit2_C_Programming/1-C_Basics/Assignment_1/Ex_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+
+void main() 
+{ 
+    int num;
+    printf("Enter a integer: ");
+    fflush(stdout);fflush(stdin);
+    scanf("%d",&num);
+    printf("You entered: %d",num);
+
+} 
